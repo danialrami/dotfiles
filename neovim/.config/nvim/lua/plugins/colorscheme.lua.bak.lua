222f1c84b148f499bbc23768c42e4661fd0ac7ce
Daniel Ramirez <daniel@lufs.audio>
2025-11-03 22:30:09 -0600

    updated nvim

---
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua.bak.lua
@@ -0,0 +1,23 @@
+return {
+    {
+      "catppuccin/nvim",
+      lazy = false,
+      priority = 1000,
+      name = "catppuccin",
+      opts = {
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = false,
+      },
+    },
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin",
+      },
+    },
+  }
+  
