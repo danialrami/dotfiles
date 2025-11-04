39c09888c486700932f776407713d74df791ba9c
Daniel Ramirez <daniel@lufs.audio>
2025-11-03 21:54:04 -0600

    Switch to LazyVim as primary config, archive old config as nvim-lufs

--- neovim/.config/nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  require("plugins.catppuccin"),
-  require("plugins.telescope"),
-  require("plugins.treesitter"),
-  require("plugins.neotree"),
-  require("plugins.lsp-config"),
-  require("plugins.lualine"),
-}
-
